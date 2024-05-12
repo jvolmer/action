1c71d62c6ae17225adab3224b2ce3ceb131ce45c
jvolmer <juliavolmer@t-online.de>
2024-05-12 11:07:25 +0200

    Move tests in main into integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use action::model::action::Action;
 use action::model::observables::SpinLattice;
-use action::model::spin::Spin;
 use action::monte_carlo::metropolis_step::MonteCarloSimulation;
 
 fn main() {
@@ -8,12 +7,6 @@ fn main() {
 
     const SIZE: usize = 5;
     let mut lattice = SpinLattice::<SIZE>::new();
-
-    println!(
-        "Old lattice:\n{}, m = {:?}",
-        lattice,
-        lattice.magnetization()
-    );
 
     for i in 1..SIZE {
         for j in 1..SIZE {
@@ -24,28 +17,4 @@ fn main() {
             lattice.sites[i][j] = simulation.step(action).update(lattice.sites[i][j]);
         }
     }
-
-    println!(
-        "New lattice:\n{}, m = {:?}",
-        lattice,
-        lattice.magnetization()
-    );
-
-    let old_spin = Spin::Up;
-    let action = Action::local(&old_spin, vec![&Spin::Up]);
-    println!("Old Spin: {:?}, Action: {:?}", old_spin, action);
-
-    let (up_count, down_count) = (0..1000)
-        .map(|_| {
-            let step = simulation.step(action.clone());
-            step.update(old_spin.clone())
-        })
-        .fold((0, 0), |(up_count, down_count), spin| {
-            if spin == Spin::Up {
-                (up_count + 1, down_count)
-            } else {
-                (up_count, down_count + 1)
-            }
-        });
-    println!("New Spin: Up {}, Down {}", up_count, down_count);
 }
---
+++ tests/simulation_test.rs
@@ -0,0 +1,57 @@
+use action::model::action::Action;
+use action::model::observables::SpinLattice;
+use action::model::spin::Spin;
+use action::monte_carlo::metropolis_step::MonteCarloSimulation;
+
+#[test]
+fn update_lattice_once() {
+    let mut simulation = MonteCarloSimulation::new(569);
+
+    const SIZE: usize = 5;
+    let mut lattice = SpinLattice::<SIZE>::new();
+
+    println!(
+        "Old lattice:\n{}, m = {:?}",
+        lattice,
+        lattice.magnetization()
+    );
+
+    for i in 1..SIZE {
+        for j in 1..SIZE {
+            let action = Action::local(
+                lattice.site(i as isize, j as isize),
+                lattice.neighborhood(i as isize, j as isize),
+            );
+            lattice.sites[i][j] = simulation.step(action).update(lattice.sites[i][j]);
+        }
+    }
+
+    println!(
+        "New lattice:\n{}, m = {:?}",
+        lattice,
+        lattice.magnetization()
+    );
+}
+
+#[test]
+fn spin_update_often() {
+    let mut simulation = MonteCarloSimulation::new(569);
+
+    let old_spin = Spin::Up;
+    let action = Action::local(&old_spin, vec![&Spin::Up]);
+    println!("Old Spin: {:?}, Action: {:?}", old_spin, action);
+
+    let (up_count, down_count) = (0..1000)
+        .map(|_| {
+            let step = simulation.step(action.clone());
+            step.update(old_spin.clone())
+        })
+        .fold((0, 0), |(up_count, down_count), spin| {
+            if spin == Spin::Up {
+                (up_count + 1, down_count)
+            } else {
+                (up_count, down_count + 1)
+            }
+        });
+    println!("New Spin: Up {}, Down {}", up_count, down_count);
+}
